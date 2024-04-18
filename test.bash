2f69fdb4e2d125092d0513f2c81eebb350d0a9e9
Tommy Girardi <tgirardi@Tommys-MBP.localdomain>
2024-04-18 09:50:16 +0200

    test

---
+++ test.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ajoute tous les fichiers modifiés
+git add *
+
+# Fait un commit avec le message "test"
+git commit -m "test"
+
+# Pousse les changements sur la branche main
+git push origin main
+
